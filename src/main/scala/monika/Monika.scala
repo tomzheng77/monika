9e07fb144d43a57a93f91be2a19470883900fa65
Tom Zheng <tomzheng77@gmail.com>
2019-01-02 22:21:55 +1100

    use apache commons exec

--- src/main/scala/monika/Monika.scala
+++ src/main/scala/monika/Monika.scala
@@ -1,20 +1,39 @@
 package monika
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 import java.util.{Timer, TimerTask}
+
+import org.apache.commons.exec.DefaultExecutor
+import org.apache.commons.exec.PumpStreamHandler
+import org.apache.commons.exec.CommandLine
 
 object Monika {
 
-  def call(args: Array[String]): Unit = {
+  case class CommandOutput(exitValue: Int, stdout: Array[Byte], stderr: Array[Byte])
 
+  def call(program: String, args: Array[String] = Array.empty, input: Array[Byte] = Array.empty): CommandOutput = {
+    val cmd = new CommandLine(program)
+    cmd.addArguments(args)
+
+    val executor = new DefaultExecutor()
+    val stdin = new ByteArrayInputStream(input)
+    val stdout = new ByteArrayOutputStream()
+    val stderr = new ByteArrayOutputStream()
+    val psh = new PumpStreamHandler(stdout, stderr, stdin)
+    executor.setStreamHandler(psh)
+
+    val exitValue = executor.execute(cmd)
+    CommandOutput(exitValue, stdout.toByteArray, stderr.toByteArray)
   }
 
   def enableFirewallForProfile(): Unit = {
-    call("iptables -w 10 -A OUTPUT -p tcp -m owner --uid-owner profile --dport 80 -j REJECT".split(' '))
-    call("iptables -w 10 -A OUTPUT -p tcp -m owner --uid-owner profile --dport 443 -j REJECT".split(' '))
+//    call("iptables", "-w 10 -A OUTPUT -p tcp -m owner --uid-owner profile --dport 80 -j REJECT".split(' '))
+//    call("iptables", "-w 10 -A OUTPUT -p tcp -m owner --uid-owner profile --dport 443 -j REJECT".split(' '))
   }
 
   def onSecondTick(): Unit = {
-
+    val out = call("ls", Array("-la"))
+    println(new String(out.stdout, "UTF-8"))
   }
 
   def startCommandListener(): Unit = {

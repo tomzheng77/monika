b643d7335373506672b20a9d251ed8082a825cca
tomzheng <tomzheng77@gmail.com>
2019-01-03 10:49:46 +1100

    added documentation

--- src/main/scala/monika/Environment.scala
+++ src/main/scala/monika/Environment.scala
@@ -8,6 +8,16 @@ object Environment {
 
   case class CommandOutput(exitValue: Int, stdout: Array[Byte], stderr: Array[Byte])
 
+  /**
+    * calls a program either by name inside PATH or the full path
+    * blocks until execution is complete
+    * captures and returns all program output
+    *
+    * @param program either the name of the program or the full path
+    * @param args to execute the program with
+    * @param input to pass into stdin
+    * @return an object containing exit value, stdout and stderr
+    */
   def call(program: String, args: Array[String] = Array.empty, input: Array[Byte] = Array.empty): CommandOutput = {
     val cmd = new CommandLine(program)
     cmd.addArguments(args)

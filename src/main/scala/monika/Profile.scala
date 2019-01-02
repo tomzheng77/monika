86559a7606ba215b2fe8ca5dd21ede86b1339364
tomzheng <tomzheng77@gmail.com>
2019-01-03 09:42:31 +1100

    upd profile module

--- src/main/scala/monika/Profile.scala
+++ src/main/scala/monika/Profile.scala
@@ -2,7 +2,9 @@ package monika
 
 object Profile {
 
+  case class Proxy(allowHtmlPrefix: Vector[String], rejectHtmlKeywords: Vector[String])
   case class Bookmark(name: String, url: String)
-  case class Configuration(name: String, allowHtmlPrefix: String, programs: Vector[String], bookmarks: String, projects: Vector[String])
+  case class Configuration(name: String, programs: Vector[String], bookmarks: String, projects: Vector[String], proxy: Proxy)
+  case class QueueItem(configuration: Configuration, time: Int)
 
 }

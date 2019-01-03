59cc1c9401c4f88c801bf36ccc73682312a87cd7
Tom Zheng <tomzheng77@gmail.com>
2019-01-03 21:18:34 +1100

    refactor

--- src/main/scala/monika/Interpreter.scala
+++ src/main/scala/monika/Interpreter.scala
@@ -2,9 +2,9 @@ package monika
 
 import java.time.LocalDateTime
 
-import monika.Profile.{MonikaState, ProfileInQueue, ProfileMode}
+import monika.Profile.{MonikaState, ProfileInQueue, ProfileMode, ProxySettings}
 import org.json4s.native.JsonMethods
-import org.json4s.{DefaultFormats, Formats}
+import org.json4s.{DefaultFormats, Extraction, Formats}
 import scalaz.ReaderWriterState
 import spark.Spark
 
@@ -12,12 +12,57 @@ import scala.util.Try
 
 object Interpreter {
 
-  case class RunCommand(program: String, args: Vector[String])
-  type ST[T] = scalaz.ReaderWriterState[Unit, List[RunCommand], MonikaState, T]
-  type STR[T] = (List[RunCommand], T, MonikaState)
-  def RWS = ReaderWriterState
+  sealed trait Effect
+  case class RunCommand(program: String, args: Vector[String]) extends Effect
+  case class RestartProxy(settings: ProxySettings) extends Effect
 
-  def addqueue_transaction(args: List[String], nowTime: LocalDateTime): ST[String] = RWS((_, state) => {
+  type ST[T] = scalaz.ReaderWriterState[Unit, List[Effect], MonikaState, T]
+  type STR[T] = (List[RunCommand], T, MonikaState)
+  def RWS[T] = ReaderWriterState.apply[Unit, List[Effect], MonikaState, T]
+
+  def statusReport(): ST[String] = RWS((_, state) => {
+    implicit val formats: Formats = DefaultFormats
+    val response = JsonMethods.pretty(JsonMethods.render(Extraction.decompose(state)))
+    (Nil, response, state)
+  })
+
+  def dropOverdueItems(time: LocalDateTime): ST[Unit] = RWS((_, state) => {
+    (Nil, Unit, state.copy(queue = state.queue.dropWhile(item => item.endTime.isBefore(time))))
+  })
+
+  def popQueue(): ST[ProfileInQueue] = RWS((_, state) => {
+    (Nil, state.queue.head, state.copy(queue = state.queue.tail))
+  })
+
+  def readState(): ST[MonikaState] = RWS((_, state) => {
+    (Nil, state, state)
+  })
+
+  def unlockAllUsers(): ST[String] = RWS((_, state) => {
+    (Constants.Users.map(user => RunCommand("passwd", Vector("-u", user))).toList, "all users unlocked", state)
+  })
+
+  def applyProfile(profile: ProfileMode): ST[String] = RWS((_, state) => {
+    profile.name
+    (null, "", null)
+  })
+
+  def applyNextProfileInQueue(): ST[String] = for {
+    item <- popQueue()
+    response <- applyProfile(item.profile)
+  } yield response
+
+  def checkQueue(nowTime: LocalDateTime): ST[String] = for {
+    _ <- dropOverdueItems(nowTime)
+    state <- readState()
+    response <- {
+      if (state.at.isEmpty && state.queue.isEmpty) unlockAllUsers()
+      else if (state.at.isEmpty && state.queue.head.startTime.isBefore(nowTime)) applyNextProfileInQueue()
+      else unlockAllUsers()
+    }
+  } yield response
+
+  def addItemToQueue(args: List[String], nowTime: LocalDateTime): ST[String] = RWS((_, state) => {
     if (args.length != 2) (Nil, "usage: addqueue <profile> <time>", state)
     else {
       val profileName = args.head
@@ -28,15 +73,15 @@ object Interpreter {
         case None => (Nil, s"time is invalid", state)
         case Some(t) if t <= 0 => (Nil, s"time must be positive, provided $t", state)
         case Some(t) => {
-          def addToQueueStartingAt(start: LocalDateTime): STR[String] = {
-            val profile = state.profiles(profileName)
+          val profile = state.profiles(profileName)
+          def addToQueueAfter(start: LocalDateTime): STR[String] = {
             (Nil, "successfully added", state.copy(queue = Vector(
               ProfileInQueue(start, start.plusMinutes(t), profile)
             )))
           }
-          if (state.queue.isEmpty && state.at.isEmpty) addToQueueStartingAt(nowTime)
-          else if (state.queue.isEmpty) addToQueueStartingAt(state.at.get.endTime)
-          else addToQueueStartingAt(state.queue.last.endTime)
+          if (state.queue.isEmpty && state.at.isEmpty) addToQueueAfter(nowTime)
+          else if (state.queue.isEmpty) addToQueueAfter(state.at.get.endTime)
+          else addToQueueAfter(state.queue.last.endTime)
         }
       }
     }
@@ -47,11 +92,16 @@ object Interpreter {
     def addqueue(args: List[String]): String = {
       val nowTime = LocalDateTime.now()
       Storage.transaction(state => {
-        val (_, response, newState) = addqueue_transaction(args, nowTime).run(null, state)
+        val (_, response, newState) = addItemToQueue(args, nowTime).run(null, state)
         (newState, response)
       })
     }
-    def status(): String = ""
+    def status(): String = {
+      Storage.transaction(state => {
+        val (_, response, newState) = statusReport().run(null, state)
+        (newState, response)
+      })
+    }
     def resetprofile(): String = ""
   }
 

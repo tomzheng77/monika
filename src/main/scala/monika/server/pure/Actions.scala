e455e17aa381cce89583a6554903c18615342784
tomzheng <tomzheng77@gmail.com>
2019-01-05 01:35:30 +1100

    refactor

--- src/main/scala/monika/server/pure/Actions.scala
+++ src/main/scala/monika/server/pure/Actions.scala
@@ -5,11 +5,11 @@ import java.time.LocalDateTime
 import monika.server.Constants.CallablePrograms._
 import monika.server.Constants._
 import monika.server.pure.Model._
-import org.apache.log4j.Level
+import monika.server.pure.ProfileActions.restrictProfile
 import org.json4s.native.JsonMethods
 import org.json4s.{DefaultFormats, Extraction, Formats, JValue}
+import scalaz.@@
 import scalaz.syntax.id._
-import scalaz.{@@, Tag}
 
 import scala.util.Try
 
@@ -35,73 +35,6 @@ object Actions {
   def unlockAllUsers(): Action[String] = Action((_, state) => {
     (Users.map(user => RunCommand(passwd, "-u", user)), "all users unlocked", state)
   })
-
-  def makeBookmarks(bookmarks: Vector[Bookmark]): String = {
-    ""
-  }
-
-  def restrictWebsites(profile: Profile): Vector[Effect] = {
-    Vector(
-      RestartProxy(profile.proxy),
-      WriteStringToFile(FilePath(Locations.ChromeBookmark), makeBookmarks(profile.bookmarks)),
-      RunCommand(chown, ProfileUserGroup, Locations.ChromeBookmark)
-    )
-  }
-
-  def restrictProjects(ext: External, profile: Profile): Vector[Effect] = {
-    // owns the project root with main user, sets permission to 755
-    val lockProjectRootFolder: Vector[Effect] = Vector(
-      RunCommand(chmod, "755", Locations.ProjectRoot),
-      RunCommand(chown, MainUserGroup, Locations.ProjectRoot)
-    )
-    // sets each project recursively to 770
-    // owns each project recursively to profile user
-    // owns each project root to main user
-    val lockEachProjectFolder: Vector[Effect] = {
-      ext.projects.values.flatMap(projPath => Vector(
-        RunCommand(chmod, "-R", "770", Tag.unwrap(projPath)),
-        RunCommand(chown, "-R", ProfileUserGroup, Tag.unwrap(projPath)),
-        RunCommand(chown, MainUserGroup, Tag.unwrap(projPath))
-      )).toVector
-    }
-    // attempts to locate each profile project in the external environment
-    // if found, the folder becomes owned by the profile use
-    // if not, a message is included to indicate it was not found
-    val findAndUnlockEachProjectFolder: Vector[Effect] = {
-      val found = profile.projects.filter(ext.projects.contains)
-      val projects: Vector[String @@ FilePath] = found.map(ext.projects)
-      projects.map(projPath => {
-        RunCommand(chown, ProfileUserGroup, Tag.unwrap(projPath))
-      })
-    }
-    // writes a line of log for every project not found
-    val reportNotFoundProjects: Vector[Effect] = {
-      val notFound = profile.projects.filter(ext.projects.contains)
-      notFound.map(proj => WriteLog(Level.DEBUG, s"project not found: ${Tag.unwrap(proj)}"))
-    }
-    lockProjectRootFolder ++
-    lockEachProjectFolder ++
-    findAndUnlockEachProjectFolder ++
-    reportNotFoundProjects
-  }
-
-  def restrictPrograms(profile: Profile): Vector[Effect] = {
-    RunCommand(usermod, "-G", "", ProfileUser) +:
-    profile.programs.map(prog => {
-      RunCommand(usermod, "-a", "-G", s"use-${Tag.unwrap(prog)}", ProfileUser)
-    })
-  }
-
-  /**
-    * updates the proxy
-    * ensures: effects are generated to ensure the new profile mode
-    *          is put into effect for the profile user
-    */
-  def restrictProfile(ext: External, profile: Profile): Vector[Effect] = {
-    restrictWebsites(profile) ++
-    restrictProjects(ext, profile) ++
-    restrictPrograms(profile)
-  }
 
   def applyNextProfileInQueue(): Action[String] = for {
     item <- popQueue()

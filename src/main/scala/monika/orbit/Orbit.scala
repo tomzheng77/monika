9413fb86b9a917883f071997bec1e17ebb58ff5d
Tom Zheng <tomzheng77@gmail.com>
2019-01-17 13:21:29 +1100

    upd orbit

--- src/main/scala/monika/orbit/Orbit.scala
+++ src/main/scala/monika/orbit/Orbit.scala
@@ -2,4 +2,8 @@ package monika.orbit
 
 object Orbit {
 
+  // rebit <address>: pockets bitcoin from the specified address
+  // orbit <time>: at <time>, you will have 10 minutes to solve a captcha, otherwise all bitcoin will be lost
+  // withdraw <address> withdraws all bitcoin to the given address
+
 }

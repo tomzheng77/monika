0ced892a0b77e50ce7e94a701bb9ff8ba61d85d3
Tom Zheng <tomzheng77@gmail.com>
2019-01-17 17:34:44 +1100

    upd script

--- src/main/scala/monika/server/signal/SetProfile.scala
+++ src/main/scala/monika/server/signal/SetProfile.scala
@@ -2,10 +2,11 @@ package monika.server.signal
 
 import java.io.PrintWriter
 
-import monika.server.{Configuration, LittleProxy, Restrictions}
+import monika.server.{Configuration, Hibernate, LittleProxy, Restrictions}
 import monika.server.Constants.Locations
 
 object SetProfile extends Script {
+
   override def run(args: Vector[String], out: PrintWriter): Unit = {
     val profiles = Configuration.readProfileDefinitions()
     lazy val name = args.head
@@ -19,7 +20,9 @@ object SetProfile extends Script {
       Restrictions.removeFromWheelGroup()
       Restrictions.restrictProgramsExcept(profile.programs)
       Restrictions.restrictProjectsExcept(profile.projects)
+      Hibernate.transaction(state => (state.copy(proxy = profile.proxy), Unit))
       out.println("set-profile success")
     }
   }
+
 }

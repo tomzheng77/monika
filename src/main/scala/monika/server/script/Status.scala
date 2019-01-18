64a00a57af2c0b3b57e42fd82bb0cf54679766ad
Tom Zheng <tomzheng77@gmail.com>
2019-01-18 21:03:18 +1100

    improve formatting

--- src/main/scala/monika/server/script/Status.scala
+++ src/main/scala/monika/server/script/Status.scala
@@ -6,15 +6,18 @@ import monika.server.{UseDateTime, UseScalaz}
 object Status extends Script with UseScalaz with UseDateTime {
 
   override def run(args: Vector[String]): SC[Unit] = (api: ScriptAPI) => {
+    api.printLine("========== [Commands] ==========")
     for (script <- Script.allScripts) {
       script match {
-        case _: RequireRoot => api.printLine(s"${script.name} (root)")
-        case _ => api.printLine(s"${script.name}")
+        case _: RequireRoot => api.printLine(s"\t- ${script.name} (root)")
+        case _ => api.printLine(s"\t- ${script.name}")
       }
     }
+    api.printLine("")
+    api.printLine("========== [Queue] ==========")
     val state = api.query()
     for (FutureAction(at, script, args) <- state.queue) {
-      api.printLine(s"${at.format(DefaultFormatter)}: ${script.name} ${args.mkString(" ")}")
+      api.printLine(s"- ${at.format(DefaultFormatter)}: ${script.name} ${args.mkString(" ")}")
     }
   }
 

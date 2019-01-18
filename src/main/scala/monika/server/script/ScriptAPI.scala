d0e1ef804f6df327ea048096a7bd7533739c71d2
Tom Zheng <tomzheng77@gmail.com>
2019-01-18 17:32:48 +1100

    add dependencies

--- src/main/scala/monika/server/script/ScriptAPI.scala
+++ src/main/scala/monika/server/script/ScriptAPI.scala
@@ -4,19 +4,32 @@ import java.time.LocalDateTime
 
 import monika.Primitives.FileName
 import monika.server.Structs.MonikaState
+import monika.server.Subprocess.CommandOutput
+import monika.server.proxy.Filter
 import scalaz.@@
 
 trait ScriptAPI {
 
   def println(str: String): Unit
 
-  def run(script: Script, args: List[String] = List.empty)
+  /**
+    * - runs another script at the specified time
+    * - if the time is before now, it will be run immediately after the current script has completed
+    */
   def enqueue(at: LocalDateTime, script: Script, args: List[String] = List.empty)
 
-  def call(command: String @@ FileName, args: String*)
+  /**
+    * - executes a shell command and returns the output
+    */
+  def call(command: String @@ FileName, args: String*): CommandOutput
 
   def query(): MonikaState
-  def command(fn: MonikaState => MonikaState): Unit = transaction(state => (fn(state), Unit))
+  def update(fn: MonikaState => MonikaState): Unit = transaction(state => (fn(state), Unit))
   def transaction[A](fn: MonikaState => (MonikaState, A)): A
+
+  /**
+    * - restarts the proxy with the given filter
+    */
+  def restartProxy(filter: Filter): Unit
 
 }

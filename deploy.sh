21efd88739daeb54de06ba4a69baa771e3755ae2
Tom Zheng <tomzheng77@gmail.com>
2019-01-18 22:32:21 +1100

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo cp target/scala-2.12/monika.jar /home/tomzheng/monika/monika.jar
+sudo chmod 755 /home/tomzheng/monika/monika.jar
+sudo chown root:root /home/tomzheng/monika/monika.jar

276135136ac89b16d5ec392f23cfba86f26b4376
Tom Zheng <tomzheng77@gmail.com>
2019-01-18 20:40:24 +1100

    upd status

--- src/main/scala/monika/server/script/Status.scala
+++ src/main/scala/monika/server/script/Status.scala
@@ -6,6 +6,12 @@ import monika.server.{UseDateTime, UseScalaz}
 object Status extends Script with UseScalaz with UseDateTime {
 
   override def run(args: Vector[String]): SC[Unit] = (api: ScriptAPI) => {
+    for (script <- Script.allScripts) {
+      script match {
+        case _: RequireRoot => api.printLine(s"${script.name} (root)")
+        case _ => api.printLine(s"${script.name}")
+      }
+    }
     val state = api.query()
     for (FutureAction(at, script, args) <- state.queue) {
       api.printLine(s"${at.format(DefaultFormatter)}}: ${script.name} ${args.mkString(" ")}")

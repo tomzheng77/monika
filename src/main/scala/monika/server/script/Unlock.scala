7ef786a986359a892c2754f8529a8c6bdbba27f9
Tom Zheng <tomzheng77@gmail.com>
2019-01-18 21:44:28 +1100

    fix

--- src/main/scala/monika/server/script/Unlock.scala
+++ src/main/scala/monika/server/script/Unlock.scala
@@ -8,7 +8,7 @@ object Unlock extends Script with RequireRoot with RestrictionOps {
   override def run(args: Vector[String]): SC[Unit] = (api: ScriptAPI) => {
     api.restartProxy(TransparentFilter)
     api.transaction(state => (state.copy(filter = TransparentFilter, root = true), Unit))
-    clearAllRestrictions()
+    clearAllRestrictions()(api)
     api.printLine("unlock success")
   }
 

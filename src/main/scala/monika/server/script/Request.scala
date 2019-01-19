00c4e4ea3f882dd5f353f8611abcc2f93b36812d
Tom Zheng <tomzheng77@gmail.com>
2019-01-19 23:52:04 +1100

    upd

--- src/main/scala/monika/server/script/Request.scala
+++ src/main/scala/monika/server/script/Request.scala
@@ -46,11 +46,11 @@ object Request extends Script with UseDateTime {
         )), Unit)
         case Some(future) if future.script != Unlock => api.printLine("queue does not end with an unlock"); (state, Unit)
         case Some(FutureAction(at, Unlock, _)) => {
-          val newAt = at.plusMinutes(minutes)
+
           val scriptAct = FutureAction(at, script, args)
-          val unlockAct = FutureAction(newAt, Unlock)
+          val unlockAct = FutureAction(at.plusMinutes(minutes), Unlock)
           api.printLine(s"script '${script.name}' will run at ${at.format(DefaultFormatter)}")
-          api.printLine(s"unlock moved to ${newAt.format(DefaultFormatter)}")
+          api.printLine(s"unlock moved to ${unlockAct.at.format(DefaultFormatter)}")
           (state.copy(queue = state.queue.dropRight(1) :+ scriptAct :+ unlockAct), Unit)
         }
       }

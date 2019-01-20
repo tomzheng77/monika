bb78be092c053ae20ec5d1ae8771616409bc675b
Tom Zheng <tomzheng77@gmail.com>
2019-01-20 10:28:05 -0800

    impl super

--- src/main/scala/monika/super/Super.scala
+++ src/main/scala/monika/super/Super.scala
@@ -2,6 +2,12 @@ package monika.`super`
 
 object Super {
   def main(args: Array[String]): Unit = {
+    val builder = new ProcessBuilder()
+    builder.command(args: _*)
+    builder.inheritIO()
 
+    val process = builder.start()
+    val exitCode = process.waitFor()
+    System.exit(exitCode)
   }
 }

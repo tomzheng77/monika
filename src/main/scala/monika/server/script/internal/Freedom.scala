409a013c7a8cdd17221910313cc6e0e0b4fe5b1d
Tom Zheng <tomzheng77@gmail.com>
2019-01-19 20:15:48 -0800

    refactor

---
+++ src/main/scala/monika/server/script/internal/Freedom.scala
@@ -0,0 +1,14 @@
+package monika.server.script.internal
+
+import monika.server.script.Script
+import monika.server.script.property.{CanRequest, Internal}
+
+object Freedom extends Script(Internal, CanRequest) {
+
+  override def run(args: Vector[String]): SC[Unit] = steps(
+    clearAllRestrictions(),
+    removeFromWheelGroup(),
+    enqueueNextStep(ForceOut)
+  ).map(_ => Unit)
+
+}

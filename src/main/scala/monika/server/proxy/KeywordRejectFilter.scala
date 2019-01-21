9bff97f422a178f2ce83dbab2be4e2b5e925d5a9
Tom Zheng <tomzheng77@gmail.com>
2019-01-21 00:45:53 -0800

    half develop

--- src/main/scala/monika/server/proxy/KeywordRejectFilter.scala
+++ src/main/scala/monika/server/proxy/KeywordRejectFilter.scala
@@ -5,28 +5,35 @@ import monika.server.{Constants, UseLogger}
 
 case class KeywordRejectFilter(keywords: Set[String]) extends Filter with UseLogger {
   override def shouldAllow(request: HttpRequest, response: HttpResponse): Boolean = {
-    def mkHeaders(msg: HttpMessage): Map[String, String] = {
-      import scala.collection.JavaConverters._
-      msg.headers().asScala.map(entry => entry.getKey -> entry.getValue).toMap
-    }
 
     // reject if keyword found in header
-    def matchAnyIn(string: String): Boolean = {
+    val url = getURL(request)
+    val content = getContent(response)
+    val regexes = keywords.map(_.r)
+    regexes.exists(r => r.findFirstMatchIn("").nonEmpty)
+  }
 
+  private def getContent(response: HttpResponse): Option[String] = {
+    val responseHeaders = mkHeaders(response)
+    val contentType = responseHeaders.getOrElse("Content-Type", "")
+    if (!contentType.startsWith("text/html")) return true
+    response match {
+      case full: FullHttpResponse => Some(full.content().toString(Constants.GlobalCharset))
+      case _ => None
     }
+  }
 
+  private def getURL(request: HttpRequest): String = {
     val requestHeaders = mkHeaders(request)
     val host = requestHeaders.getOrElse("Host", "")
     val uri = request.getUri
     val url = if (uri.startsWith("http://") || uri.startsWith("https://")) uri else host + uri
-    val urlWithoutHTTP = url.replaceFirst("^https?://", "")
-    if (keywords.exists(urlWithoutHTTP.contains)) false
-    else response match {
-      case full: FullHttpResponse => {
-        val content = full.content().toString(Constants.GlobalCharset)
-        !keywords.exists(content.contains)
-      }
-      case _ => true
-    }
+    url.replaceFirst("^https?://", "")
   }
+
+  private def mkHeaders(msg: HttpMessage): Map[String, String] = {
+    import scala.collection.JavaConverters._
+    msg.headers().asScala.map(entry => entry.getKey -> entry.getValue).toMap
+  }
+
 }

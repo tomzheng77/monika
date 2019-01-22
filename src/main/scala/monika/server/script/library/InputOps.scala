641d5392615d036393d61bf91424895b64a440b2
Tom Zheng <tomzheng77@gmail.com>
2019-01-21 23:30:34 -0800

    upd

--- src/main/scala/monika/server/script/library/InputOps.scala
+++ src/main/scala/monika/server/script/library/InputOps.scala
@@ -3,7 +3,7 @@ package monika.server.script.library
 trait InputOps {
 
   def toSet(input: String): Set[String] = {
-    input.split(',').toSet.map(s => s.trim()).filter(_.nonEmpty)
+    input.split(',').toSet.map((s: String) => s.trim()).filter(_.nonEmpty)
   }
 
 }

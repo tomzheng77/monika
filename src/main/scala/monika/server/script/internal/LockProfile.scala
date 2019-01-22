52faf6b70c5545856c538428c1c5e7fbf17cf189
Tom Zheng <tomzheng77@gmail.com>
2019-01-21 22:46:33 -0800

    added lock-profile

---
+++ src/main/scala/monika/server/script/internal/LockProfile.scala
@@ -0,0 +1,36 @@
+package monika.server.script.internal
+
+import monika.Primitives.FileName
+import monika.server.proxy.URLFilter
+import monika.server.script.Script
+import monika.server.script.property.{CanRequest, Internal}
+
+object LockProfile extends Script(Internal, CanRequest) {
+
+  override def run(args: Vector[String]): SC[Unit] = {
+    if (args.size != 4) {
+      printLine("usage: lock-profile <url-allow> <url-reject> <programs> <projects>")
+    } else {
+      val urlAllow = args(0).split(',').toSet
+      val urlReject = args(1).split(',').toSet
+      val programs = args(2).split(',').toSet
+      val projects = args(3).split(',').toSet
+      lockProfileInternal(urlAllow, urlReject, programs, projects)
+    }
+  }
+
+  private def lockProfileInternal(
+    urlAllow: Set[String],
+    urlReject: Set[String],
+    programs: Set[String],
+    projects: Set[String]
+  ): SC[Unit] = steps(
+    clearAllRestrictions(),
+    setNewFilter(URLFilter(urlAllow, urlReject)),
+    setAsNonRoot(),
+    restrictProgramsExcept(programs.map(FileName).toVector),
+    restrictProjectsExcept(projects.map(FileName).toVector),
+    enqueueNextStep(ForceOut)
+  )
+
+}

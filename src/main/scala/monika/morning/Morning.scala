55d53328a1a61a15f9c590dca5b31058f56ef836
tom <tom@onmicrosoft.com>
2019-01-30 04:50:09 -0500

    MORNING!

--- src/main/scala/monika/morning/Morning.scala
+++ src/main/scala/monika/morning/Morning.scala
@@ -1,5 +1,8 @@
 package monika.morning
 
+import monika.server.Constants
+import spark.Spark
+
 object Morning {
 
   // - run an HTTP server
@@ -7,9 +10,26 @@ object Morning {
   // - user can deposit a note at any time
   // - at each day 7 AM, if there are no captcha solves in the previous hour, then
   //   all notes will be deleted
+  // - notes cannot be viewed from 11 PM until 7 AM
 
   def main(args: Array[String]): Unit = {
-
+    Spark.port(Constants.InterpreterPort)
+    Spark.get("/captcha", (req, resp) => {
+      // TODO: provide a CAPTCHA image for the user to solve
+      ""
+    })
+    Spark.get("/add-note", (req, resp) => {
+      // TODO: remember the note
+      ""
+    })
+    Spark.get("/view-notes", (req, resp) => {
+      // TODO: print out all notes
+      ""
+    })
+    Spark.get("/remove-note", (req, resp) => {
+      // TODO: remove the note
+      ""
+    })
   }
 
 }

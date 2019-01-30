a258d168b30b3865d6338afabe6e196d9ec84d6f
tom <tom@onmicrosoft.com>
2019-01-30 04:46:31 -0500

    upd morning

---
+++ src/main/scala/monika/morning/Morning.scala
@@ -0,0 +1,15 @@
+package monika.morning
+
+object Morning {
+
+  // - run an HTTP server
+  // - user can solve a captcha at any time, the timestamp will be recorded
+  // - user can deposit a note at any time
+  // - at each day 7 AM, if there are no captcha solves in the previous hour, then
+  //   all notes will be deleted
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}

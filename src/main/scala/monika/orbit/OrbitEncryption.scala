9f04d9839167fd0296c28f3c91345a40fc3df0ff
tom <tom@onmicrosoft.com>
2019-02-12 09:39:59 +1100

    upd

--- src/main/scala/monika/orbit/OrbitEncryption.scala
+++ src/main/scala/monika/orbit/OrbitEncryption.scala
@@ -5,11 +5,11 @@ trait OrbitEncryption {
   private val SecretKey = "%%QLWaMKt2w^MWxN]H&0"
 
   protected def encrypt(in: String): String = {
-    ""
+    in
   }
 
   protected def decrypt(in: String): String = {
-    ""
+    in
   }
 
 }

7962d8d3651706465839333c9c62258ed6cbdff2
tom <tom@onmicrosoft.com>
2019-02-12 11:28:35 +1100

    upd encryption

--- src/test/scala/monika/orbit/OrbitEncryptionSpec.scala
+++ src/test/scala/monika/orbit/OrbitEncryptionSpec.scala
@@ -1,8 +1,10 @@
 package monika.orbit
 
+import org.json4s.jackson.JsonMethods._
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
+import org.json4s.JsonDSL._
 
 object OrbitEncryptionSpec extends Properties("OrbitEncryption") with OrbitEncryption {
 
@@ -14,6 +16,9 @@ object OrbitEncryptionSpec extends Properties("OrbitEncryption") with OrbitEncry
 
   property("encrypt and decrypt with salt") = {
     forAll(arbitrary[String])(a => {
+      println(pretty(render(a)))
+      println(pretty(render(encryptWithSalt(a))))
+      println(pretty(render(decryptWithSalt(encryptWithSalt(a)))))
       decryptWithSalt(encryptWithSalt(a)) == a
     })
   }

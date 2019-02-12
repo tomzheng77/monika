62907b415c9cf8608c9f228276670cf145838114
tom <tom@onmicrosoft.com>
2019-02-12 11:13:27 +1100

    upd encryption

--- src/main/scala/monika/orbit/OrbitEncryption.scala
+++ src/main/scala/monika/orbit/OrbitEncryption.scala
@@ -1,15 +1,25 @@
 package monika.orbit
 
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
 trait OrbitEncryption {
 
   private val SecretKey = "%%QLWaMKt2w^MWxN]H&0"
+  private val Encoding = "UTF-8"
 
   protected def encrypt(in: String): String = {
-    in
+    val aesKey = new SecretKeySpec(SecretKey.getBytes(Encoding), "AES")
+    val cipher = Cipher.getInstance("AES")
+    cipher.init(Cipher.ENCRYPT_MODE, aesKey)
+    new String(cipher.doFinal(in.getBytes(Encoding)), Encoding)
   }
 
   protected def decrypt(in: String): String = {
-    in
+    val cipher = Cipher.getInstance("AES")
+    val aesKey = new SecretKeySpec(SecretKey.getBytes(Encoding), "AES")
+    cipher.init(Cipher.DECRYPT_MODE, aesKey)
+    new String(cipher.doFinal(in.getBytes(Encoding)), Encoding)
   }
 
 }

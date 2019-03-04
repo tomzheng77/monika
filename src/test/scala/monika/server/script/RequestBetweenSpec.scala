79057daa704d899efedc666911d2a9a0ee458ec4
tom <tom@onmicrosoft.com>
2019-03-04 23:44:11 +1100

    added test

--- src/test/scala/monika/server/script/RequestBetweenSpec.scala
+++ src/test/scala/monika/server/script/RequestBetweenSpec.scala
@@ -93,6 +93,23 @@ class RequestBetweenSpec extends FlatSpec with Matchers with UseDateTime {
     ))
   }
 
+  it should "handle overlaps with end" in {
+    val state = MonikaState(queue = Vector(
+      Action(parseDateTime("2019-03-04 20:00:00").get, Freedom),
+      Action(parseDateTime("2019-03-04 21:00:00").get, Unlock)
+    ))
+    val newState = RequestBetween.requestBetweenInternal(
+      parseDateTime("2019-03-04 21:00:00").get,
+      parseDateTime("2019-03-04 23:20:00").get
+    )(LockProfile, Vector("", "", "google-chrome"))(state)
+
+    newState.queue should be(Vector(
+      Action(parseDateTime("2019-03-04 20:00:00").get, Freedom),
+      Action(parseDateTime("2019-03-04 21:00:00").get, LockProfile, Vector("", "", "google-chrome")),
+      Action(parseDateTime("2019-03-04 23:20:00").get, Unlock)
+    ))
+  }
+
   "removeDuplicate" should "remove the second script if the one before is the same" in {
     val out = RequestBetween.removeDuplicate(List(
       Action(parseDateTime("2019-03-04 18:30:00").get, Brick),

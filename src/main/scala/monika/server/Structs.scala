656e52d0ff22dd0639a0d59243ccb35af6c02216
tom <tom@onmicrosoft.com>
2019-03-04 20:40:26 +1100

    impl request-between

--- src/main/scala/monika/server/Structs.scala
+++ src/main/scala/monika/server/Structs.scala
@@ -1,27 +1,24 @@
 package monika.server
 
-import java.time.{LocalDateTime, ZoneOffset}
+import java.time.LocalDateTime
 
 import monika.server.proxy.{Filter, TransparentFilter}
 import monika.server.script.Script
-import monika.server.script.internal.Unlock
 
 object Structs {
-
-  private val Epoch = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC)
 
   /**
     * Things Monika must remember across multiple sessions
     * @param root whether the user can run privileged scripts
     * @param queue actions to perform in the future
     * @param filter settings which the proxy was last set to
-    * @param lastAction the last action that was performed
+    * @param previous the last action that was performed
     */
   case class MonikaState(
     root: Boolean = true,
     queue: Vector[Action] = Vector.empty,
     filter: Filter = TransparentFilter,
-    lastAction: Action = Action(Epoch, Unlock)
+    previous: Option[Action] = None
   )
 
   case class Action(at: LocalDateTime, script: Script, args: Vector[String] = Vector.empty)
--- src/main/scala/monika/server/script/RequestBetween.scala
+++ src/main/scala/monika/server/script/RequestBetween.scala
@@ -1,6 +1,6 @@
 package monika.server.script
 
-import java.time.LocalDateTime
+import java.time.{LocalDateTime, ZoneOffset}
 
 import monika.server.Structs.{Action, MonikaState}
 import monika.server.UseDateTime
@@ -28,21 +28,34 @@ object RequestBetween extends Script with UseDateTime {
     }
   }
 
-  private def isQueue(script: Script) = {
-    script.hasProperty(Mainline)
-    val queue = Set(Brick, Freedom, LockProfile, Unlock)
-  }
+  private val Epoch = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC)
 
-  private def trn(
-    from: LocalDateTime,
-    to: LocalDateTime)
+  private def trn(start: LocalDateTime, end: LocalDateTime)
     (script: Script, args: Vector[String])
     (state: MonikaState): MonikaState = {
 
+    if (!end.isAfter(start)) return state
+
+    val previous: Action = state.previous.filter(a ⇒ a.script.hasProperty(Mainline)).getOrElse(Action(Epoch, Unlock))
     val mainline: List[Action] = state.queue.filter(a ⇒ a.script.hasProperty(Mainline)).toList
     val notMainline: List[Action] = state.queue.filter(a ⇒ a.script.hasProperty(Mainline)).toList
 
-    null
+    val beforeStart = mainline.takeWhile(_.at.isBefore(start))
+    val atOrAfterEnd = mainline.dropWhile(_.at.isBefore(end))
+    val inBetween = mainline.slice(beforeStart.length, mainline.length - atOrAfterEnd.length)
+
+    // check if the script intersects with a non-freedom action
+    val typeAtStart = beforeStart.lastOption.getOrElse(previous)
+    val isBlocked = (typeAtStart :: inBetween).exists(a ⇒ a.script != Unlock && a.script != Freedom)
+    if (isBlocked) return state
+
+    val newMainline = beforeStart ++ List(Action(start, script, args)) ++ {
+      if (atOrAfterEnd.isEmpty) List(Action(end, Unlock, args))
+      else if (atOrAfterEnd.head.at.isEqual(end)) Nil
+      else List(Action(end, Freedom, args))
+    } ++ atOrAfterEnd
+
+    state.copy(queue = (newMainline ++ notMainline).sortBy(_.at).toVector)
   }
 
 }
